7e78b562db200fcf9d35229dd323738f465b873c
tommiedevelops <thomas.dizon25@gmail.com>
2026-06-10 11:53:36 +1000

    added: declaration for serializeJSON in SceneGraph

--- src/client/scene/scene_graph.ts
+++ src/client/scene/scene_graph.ts
@@ -1,10 +1,22 @@
 import { Transform } from "./transform.js"
-import { Material } from "./material.js"
+import { Material } from "../assets/material.js"
 
 export default class SceneGraph
 {
+    private root : SceneNode = new SceneNode();
+
     constructor(private pathToJSON : string = "")
-    {}
+    {
+        // recursively construct SceneGraph from JSON file
+        // validate JSON file
+        // use a JSON Schema to ensure consistent structure
+    }
+
+    public serializeJSON(pathToSeriralize : string) : void
+    {
+        // method for serializing the SceneGraph to JSON
+    }
+
 }
 
 class SceneNode
