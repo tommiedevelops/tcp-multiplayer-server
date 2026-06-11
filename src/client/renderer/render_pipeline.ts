4278d62fd6da9080305caa1dff59737b56c4ba96
tommiedevelops <thomas.dizon25@gmail.com>
2026-06-11 14:48:23 +1000

    refactor: changed file structure (again)

--- src/client/renderer/render_pipeline.ts
+++
@@ -1,4 +0,0 @@
-export class RenderPipeline
-/* Describes all the actions a GPU will perform when acting on a set of data */
-{
-}
--- src/client/scene/transform.ts
+++
@@ -1,8 +0,0 @@
-import { Vec3, Quat, vec3, quat } from "wgpu-matrix"
-
-export class Transform
-{
-    public position : Vec3 = vec3.create(0,0,0);
-    public rotation : Quat = quat.create(0,0,0,0);
-    public scale    : Vec3 = vec3.create(1,1,1);
-}
