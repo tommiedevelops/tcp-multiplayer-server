331861c45e295137e5c9a54d27904fec2c9278e0
tommiedevelops <thomas.dizon25@gmail.com>
2026-07-16 01:19:54 +0000

    added: outline for establishing basic echo server

--- server/main.cpp
+++ server/main.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
+
 #include <sys/socket.h>
+#include <netdb.h>
+#include <string.h>
 
 int main()
 {
-	std::cout << "Hello World!\n";
+	// Create a socket
+	// Bind the socket to an IP / port
+	// Mark the socket for listening
+	// Accept a call
+	// Close the listening socket
+	// While receiving, display message, echo message
+	// Close socket
+	return 0;
 }
